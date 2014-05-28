a7b0be6a93bb65f498d4ec2a4710f7404c6dcf88
Dan Murphy <murphydbuffalo@gmail.com>
2014-05-28 13:58:25 -0400

    Added links between all pages.

--- server.rb
+++ server.rb
@@ -18,7 +18,7 @@ end
 
 get "/movies" do
   @movies = db_connect do |conn| conn.exec(
-  	"SELECT movies.title, movies.year, movies.rating, genres.name AS genre, 
+  	"SELECT movies.title, movies.year, movies.rating, movies.id, genres.name AS genre, 
   	 studios.name AS studio FROM movies JOIN genres ON movies.genre_id = genres.id 
   	 JOIN studios ON movies.studio_id = studios.id ORDER BY movies.title LIMIT 25")
   end
@@ -29,8 +29,8 @@ get "/movies/:movie_id" do
   @movie_id = params[:movie_id]
   @movie = db_connect do |conn| conn.exec(
   	"SELECT movies.title, movies.rating, movies.year, movies.synopsis, 
-  	 studios.name AS studio, genres.name AS genre, actors.name AS actor 
-  	 FROM movies JOIN studios ON movies.studio_id = studios.id 
+  	 studios.name AS studio, genres.name AS genre, actors.name AS actor, 
+  	 actors.id FROM movies JOIN studios ON movies.studio_id = studios.id 
   	 JOIN genres ON movies.genre_id = genres.id JOIN cast_members ON
   	 cast_members.movie_id = movies.id JOIN actors ON actors.id = 
   	 cast_members.actor_id WHERE movies.id = #{@movie_id}")
@@ -40,7 +40,7 @@ end
 
 get "/actors" do
   @actors = db_connect do |conn| conn.exec(
-  	"SELECT name FROM actors\n
+  	"SELECT name, id FROM actors\n
   	 ORDER BY name LIMIT 25")
   end
   erb :"/actors/index"
@@ -49,8 +49,8 @@ end
 get "/actors/:actor_id" do
   @actor_id = params[:actor_id]
   @actor = db_connect do |conn| conn.exec(
-    "SELECT actors.name, movies.title, cast_members.character FROM actors
-     JOIN cast_members ON actors.id = cast_members.actor_id
+    "SELECT actors.name, movies.title, movies.id, cast_members.character 
+     FROM actors JOIN cast_members ON actors.id = cast_members.actor_id
      JOIN movies ON cast_members.movie_id = movies.id
      WHERE actors.id = #{@actor_id} ORDER BY movies.title")
   end
