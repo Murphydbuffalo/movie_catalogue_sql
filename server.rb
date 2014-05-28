94982f8626252650a2f6b1686ce5aed41e27fbbb
Dan Murphy <murphydbuffalo@gmail.com>
2014-05-28 14:47:52 -0400

    Added pagination, limited to "Next Page", "Previous Page" and "Home" options at the moment.

--- server.rb
+++ server.rb
@@ -17,10 +17,18 @@ get "/" do
 end
 
 get "/movies" do
+  @page = params[:page].to_i || 1
+  @page > 1 ? @offset_count = @page - 1 : @offset_count = 0
   @movies = db_connect do |conn| conn.exec(
-  	"SELECT movies.title, movies.year, movies.rating, movies.id, genres.name AS genre, 
-  	 studios.name AS studio FROM movies JOIN genres ON movies.genre_id = genres.id 
-  	 JOIN studios ON movies.studio_id = studios.id ORDER BY movies.title LIMIT 25")
+  	"SELECT movies.title, movies.year, movies.rating, movies.id, 
+  	 genres.name AS genre, studios.name AS studio 
+  	 FROM movies 
+  	 JOIN genres ON movies.genre_id = genres.id 
+  	 JOIN studios ON movies.studio_id = studios.id 
+  	 ORDER BY movies.title 
+  	 LIMIT 20
+  	 OFFSET (20 * #{@offset_count})"
+  	 )
   end
   erb :"/movies/index"
 end
@@ -30,18 +38,27 @@ get "/movies/:movie_id" do
   @movie = db_connect do |conn| conn.exec(
   	"SELECT movies.title, movies.rating, movies.year, movies.synopsis, 
   	 studios.name AS studio, genres.name AS genre, actors.name AS actor, 
-  	 actors.id FROM movies JOIN studios ON movies.studio_id = studios.id 
-  	 JOIN genres ON movies.genre_id = genres.id JOIN cast_members ON
-  	 cast_members.movie_id = movies.id JOIN actors ON actors.id = 
-  	 cast_members.actor_id WHERE movies.id = #{@movie_id}")
+  	 actors.id, cast_members.character 
+  	 FROM movies 
+  	 JOIN studios ON movies.studio_id = studios.id 
+  	 JOIN genres ON movies.genre_id = genres.id 
+  	 JOIN cast_members ON cast_members.movie_id = movies.id 
+  	 JOIN actors ON actors.id = cast_members.actor_id 
+  	 WHERE movies.id = #{@movie_id}"
+  	 )
   end
   erb :"/movies/show"
 end
 
 get "/actors" do
+  @page = params[:page].to_i || 1
+  @page > 1 ? @offset_count = @page - 1 : @offset_count = 0
   @actors = db_connect do |conn| conn.exec(
-  	"SELECT name, id FROM actors\n
-  	 ORDER BY name LIMIT 25")
+  	"SELECT name, id FROM actors
+  	 ORDER BY name 
+  	 LIMIT 20
+  	 OFFSET (20 * #{@offset_count})"
+  	 )
   end
   erb :"/actors/index"
 end
@@ -50,9 +67,12 @@ get "/actors/:actor_id" do
   @actor_id = params[:actor_id]
   @actor = db_connect do |conn| conn.exec(
     "SELECT actors.name, movies.title, movies.id, cast_members.character 
-     FROM actors JOIN cast_members ON actors.id = cast_members.actor_id
+     FROM actors 
+     JOIN cast_members ON actors.id = cast_members.actor_id
      JOIN movies ON cast_members.movie_id = movies.id
-     WHERE actors.id = #{@actor_id} ORDER BY movies.title")
+     WHERE actors.id = #{@actor_id} 
+     ORDER BY movies.title"
+     )
   end
   erb :"/actors/show"
 end
